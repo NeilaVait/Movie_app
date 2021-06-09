c3ebcf594d8856aaf08503507f0f2e11b037b007
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-09 11:47:50 +0300

    generate movie title list

---
+++ src/components/movieTable.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class MovieTable extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Movie table is here</h3>
+        <ol>
+          {this.state.movies.map((m) => (
+            <li key={m._id}>
+              <p>{m.title}</p>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default MovieTable;

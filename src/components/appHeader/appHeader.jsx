1021e69cd97c2355ccab4ab93b3eb59d9bd3d678
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-11 10:04:06 +0300

    hero img and css

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+//import style
+import './style.css';
+//add image
+import heroImg from './hero.jpg';
 
 class AppHeader extends Component {
   state = {};
   render() {
-    return <h2>I am header</h2>;
+    return (
+      <header>
+        <img className="hero" src={heroImg} alt="hero" />
+      </header>
+    );
   }
 }
 

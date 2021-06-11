8de812da6f45cc19cfae9af29151b971afc27e4f
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-11 10:52:19 +0300

    get current time done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ import AppAddTodo from './components/appAddTodo/appAddTodo';
 import './App.css';
 
 class App extends Component {
-  state = {};
+  state = {
+    todos: [
+      { id: 1, isDone: false, title: 'Buy milk' },
+      { id: 2, isDone: false, title: 'Buy tv' },
+      { id: 3, isDone: false, title: 'Go to park' },
+      { id: 4, isDone: false, title: 'Learn react' },
+    ],
+  };
   render() {
     return (
       <div className="App">
--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -6,10 +6,18 @@ import heroImg from './hero.jpg';
 
 class AppHeader extends Component {
   state = {};
+
+  getDate() {
+    const now = new Date();
+    return now.toLocaleString();
+  }
+
   render() {
     return (
       <header>
         <img className="hero" src={heroImg} alt="hero" />
+        {/* gauti siandienos data */}
+        <p className="hero-date">{this.getDate()}</p>
       </header>
     );
   }
